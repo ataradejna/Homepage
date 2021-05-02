ef5c4451a9c4acb04c71dd25842f3f96fef19794
Agnieszka Taradejna <agnieszka.taradejna@gmail.com>
2021-05-02 13:25:58 +0200

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Cześć!");
+    };
 
-let button = document.querySelector(".button");
-let imageContainer = document.querySelector(".imageContainer");
-let body = document.querySelector(".body")
-let backgroundButton = document.querySelector(".backgroundButton")
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        body.classList.toggle("background--special");
+    };
 
-button.addEventListener("click", () => {
-    if (button.innerText == "Schowaj mnie") {
-        imageContainer.classList.add("hidden");
-        button.innerText = "Pokaż mnie"
-    }
-    else if (button.innerText === "Pokaż mnie") {
-        imageContainer.classList.remove("hidden")
-        button.innerText = "Schowaj mnie";
-    }
-});
+    const hideImage = () => {
+        const imageContainer = document.querySelector(".container__image");
+        imageContainer.classList.toggle("hidden");
+        imageButton.innerText = imageContainer.classList.contains("hidden") ? "Pokaż mnie" : "Schowaj mnie";
+    };
 
-backgroundButton.addEventListener("click", () => {
-    body.classList.toggle("secondBackground")
-})
+    const init = () => {
+        const imageButton = document.querySelector(".js-imageButton");
+        const backgroundButton = document.querySelector(".js-backgroundButton"); //dlaczego nie muszę deklarować imageButton w funkcji hideImage skoro w tej funkcji zmieniam innerTekst?
+        backgroundButton.addEventListener("click", toggleBackground);
+        imageButton.addEventListener("click", hideImage);
+    };
+    
+    welcome();
+
+    init();
+}
+
+
+
+
+
+
+
+
+
+
